93e86940dcbf6461a91e0b66e2830887af3c8b95
Jan Czarina Javier <roger.palabasan@baruchmail.cuny.edu>
2019-06-01 22:26:28 -0400

    Register Users

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -9,5 +9,6 @@ namespace DatingApp.API.Dtos
         [Required]
         [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password between 4 and 8 characters")]
         public string Password { get; set; }
+        
     }
 }
