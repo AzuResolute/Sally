4e5c5960264cf467595c0a642266c77daca642af
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-06-04 21:20:02 -0400

    Angular - Added Router Navigate

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { logging } from 'protractor';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,7 @@ import { AlertifyService } from '../_services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(public authService: AuthService, private alertify: AlertifyService) { }
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -21,7 +22,10 @@ export class NavComponent implements OnInit {
       this.alertify.success('Logged in Successfully');
     }, error => {
       this.alertify.error(error);
-    });
+    }, () => {
+      this.router.navigate(['/members']);
+    }
+    );
   }
 
   loggedIn() {
@@ -31,6 +35,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('Logged out');
+    this.router.navigate(['home']);
   }
 
 }
