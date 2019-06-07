f46266c4adbf0c2653b1a77fbec65a0df4cf1120
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-06-07 16:36:11 -0400

    ASP Net - Setting Up PhotoCreation Dto and Controller dependency

--- DatingApp.API/Controllers/PhotosController.cs
+++ DatingApp.API/Controllers/PhotosController.cs
@@ -1,11 +1,17 @@
 using AutoMapper;
 using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using System.Threading.Tasks;
+using System.Security.Claims;
+using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
 using DatingApp.API.Data;
+using DatingApp.API.Dtos;
 using DatingApp.API.Helpers;
+using DatingApp.API.Models;
 
 namespace DatingApp.API.Controllers
 {
@@ -17,7 +23,10 @@ namespace DatingApp.API.Controllers
         private readonly IMapper _mapper;
         private readonly IOptions<CloudinarySettings> _cloudinaryConfig;
         private Cloudinary _cloudinary;
-        public PhotosController (IDatingRepository repo, IMapper mapper, IOptions<CloudinarySettings> cloudinaryConfig)
+        public PhotosController (
+            IDatingRepository repo,
+            IMapper mapper,
+            IOptions<CloudinarySettings> cloudinaryConfig)
         {
             _repo = repo;
             _mapper = mapper;
@@ -30,6 +39,57 @@ namespace DatingApp.API.Controllers
             );
 
             _cloudinary = new Cloudinary(acc);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddPhotoForUser(int userId, PhotosForCreationDto photosForCreationDto) {
+            // if(id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)) {
+            //     return Unauthorized();
+            // }
+            // var userFromRepo = await _repo.GetUser(id);
+
+            if(userId != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value)) {
+                return Unauthorized();
+            }
+
+            var userFromRepo = await _repo.GetUser(userId);
+            var file = photosForCreationDto.File;
+            
+            var uploadResult = new ImageUploadResult();
+
+            if(file.Length > 0) {
+                using(var stream = file.OpenReadStream()){
+                    var uploadParams = new ImageUploadParams() {
+                        File = new FileDescription(file.Name, stream),
+                        Transformation = new Transformation()
+                            .Width(500)
+                            .Height(500)
+                            .Crop("fill")
+                            .Gravity("face")
+                    };
+
+                    uploadResult = _cloudinary.Upload(uploadParams);
+                }
+            }
+
+            photosForCreationDto.Url = uploadResult.Uri.ToString();
+            photosForCreationDto.PublicID = uploadResult.PublicId;
+
+            // _mapper.Map<Destinatio>(Source);
+            var photo = _mapper.Map<Photo>(photosForCreationDto);
+
+            if(userFromRepo.Photos.Any(u => u.IsMain)) {
+                photo.IsMain = true;
+            }
+
+            userFromRepo.Photos.Add(photo);
+
+            if(await _repo.SaveAll()){
+                return Ok();
+            }
+
+            return BadRequest("Could not add the photo");
+
         }
 
     }
