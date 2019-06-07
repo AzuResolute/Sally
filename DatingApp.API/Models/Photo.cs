9138d5d1c4ee7880de8140561f23c6aff7584c03
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-06-06 23:21:22 -0400

    Set Up Cloudinary

--- DatingApp.API/Models/Photo.cs
+++ DatingApp.API/Models/Photo.cs
@@ -9,6 +9,7 @@ namespace DatingApp.API.Models
         public string Description { get; set; }
         public DateTime DateAdded { get; set; }
         public bool IsMain { get; set; }
+        public string PublicID { get; set; }
 
         //Sets up relationship with User
         public User User { get; set; }
