1673897dce10c24e7250ad9b9844d68db8ba17ef
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-06-11 15:02:08 -0400

    Angular - Added Register Validations

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -18,10 +18,10 @@ export class RegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registerForm = new FormGroup({
-      username: new FormControl,
-      password: new FormControl,
-      confirmPassword: new FormControl()
-    });
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required])
+    },this.passwordMatchValidator);
   }
 
   register() {
@@ -31,6 +31,11 @@ export class RegisterComponent implements OnInit {
     //   this.alertify.error(error);
     // });
     console.log(this.registerForm.value);
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ?
+      null : {'mismatch': true};
   }
 
   cancel() {
