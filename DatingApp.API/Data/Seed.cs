80b9614ecaa7e01f137eb824d469f7b7b2ecb768
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-06-18 16:42:07 -0400

    Fixing Bug - Seed Duplicates

--- DatingApp.API/Data/Seed.cs
+++ DatingApp.API/Data/Seed.cs
@@ -1,6 +1,7 @@
 using Newtonsoft.Json;
 using System.Collections.Generic;
 using DatingApp.API.Models;
+using DatingApp.API.Controllers;
 
 namespace DatingApp.API.Data
 {
@@ -13,20 +14,22 @@ namespace DatingApp.API.Data
         }
         
         public void SeedUsers() {
-            var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
-            var users = JsonConvert.DeserializeObject<List<User>>(userData);
-            foreach(var user in users) {
-                byte[] passwordHash, passwordSalt;
-                CreatePasswordHash("password", out passwordHash, out passwordSalt);
-                
-                user.PasswordHash = passwordHash;
-                user.PasswordSalt = passwordSalt;
-                user.Username = user.Username.ToLower();
 
-                _context.Users.Add(user);
-            }
+                var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+                var users = JsonConvert.DeserializeObject<List<User>>(userData);
+                foreach(var user in users) {
+                    byte[] passwordHash, passwordSalt;
+                    CreatePasswordHash("password", out passwordHash, out passwordSalt);
+                    
+                    user.PasswordHash = passwordHash;
+                    user.PasswordSalt = passwordSalt;
+                    user.Username = user.Username.ToLower();
 
-            _context.SaveChanges();
+                    _context.Users.Add(user);
+                }
+
+                _context.SaveChanges();
+
         }
 
         private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt){
