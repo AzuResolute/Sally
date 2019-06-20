26a29c0560075d8f0b278cf142b61cf8b82a042a
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-06-19 23:27:36 -0400

    Completed Recommendation ts

--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from '../_services/auth.service';
 import { User } from '../_models/user';
+import { PaginatedResult } from '../_models/pagination';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-home',
@@ -11,16 +14,25 @@ import { User } from '../_models/user';
 export class HomeComponent implements OnInit {
   registerMode = false;
   currentUser: User = null;
+  userParams: any = {};
   matchedUser: User = null;
   matchedNoun = 'them';
 
-  constructor(private http: HttpClient, private authService: AuthService) { }
+  constructor(
+    private http: HttpClient,
+    private authService: AuthService,
+    private userService: UserService,
+    private alertify: AlertifyService) { }
 
   ngOnInit() {
     const currentUser = this.authService.currentUser;
-    console.log(currentUser);
     if (currentUser) {
       this.currentUser = currentUser;
+      this.userParams.gender = this.currentUser.gender === 'female' ? 'male' : 'female';
+      this.userParams.minAge = 18;
+      this.userParams.maxAge = 65;
+      this.loadRecommendation();
+
       switch (currentUser.gender) {
         case 'male':
           this.matchedNoun = 'him';
@@ -31,7 +43,7 @@ export class HomeComponent implements OnInit {
         default:
           this.matchedNoun = 'them';
           break;
-      }
+        }
     } else {
       this.currentUser = null;
       this.matchedNoun = 'them';
@@ -44,6 +56,20 @@ export class HomeComponent implements OnInit {
 
   cancelRegisterMode(registerMode: boolean) {
     this.registerMode = registerMode;
+  }
+
+  loadRecommendation() {
+    this.userService.getUsers(
+      null,
+      null,
+      this.userParams).subscribe((response: PaginatedResult<User[]>) => {
+        const users = response.result;
+        console.log('users ----> ', users);
+        this.matchedUser = users[users.length - 1];
+      }, error => {
+        this.alertify.error(error);
+      }
+    );
   }
 
 }
