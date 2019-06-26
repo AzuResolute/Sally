f22ff7579d2c89850557a63ee67ae8c493f576bb
AzuResolute <roger.palabasan@baruchmail.cuny.edu>
2019-06-26 13:03:14 -0400

    Reconfiguring D3 bubbles to include Tech Stack

--- DatingApp-SPA/src/app/recommendations/recommendations.component.ts
+++ DatingApp-SPA/src/app/recommendations/recommendations.component.ts
@@ -18,48 +18,12 @@ export class RecommendationsComponent implements OnInit {
   users: User[];
   matchedUser: User = null;
   matchedNoun = 'them';
-  // nodes: Node[];
-  // links: Link[];
-  // CONFIG = {
-  //   N : 100,
-  //   SPECTRUM: [
-  //     // "rgb(222,237,250)"
-  //     'rgb(176,212,243)',
-  //     'rgb(128,186,236)',
-  //     'rgb(77,158,228)',
-  //     'rgb(38,137,223)',
-  //     'rgb(0,116,217)',
-  //     'rgb(0,106,197)'
-  //     // "rgb(0,94,176)"
-  //     // "rgb(0,82,154)"
-  //     // "rgb(0,60,113)"
-  //   ]
-  // };
 
   constructor(
     private authService: AuthService,
     private userService: UserService,
     private alertify: AlertifyService
-  ) {
-    // const N = this.CONFIG.N,
-    // getIndex = number => number - 1;
-
-    // /** constructing the nodes array */
-    // for (let i = 1; i <= N; i++) {
-    //   this.nodes.push(new Node(i));
-    // }
-
-    // for (let i = 1; i <= N; i++) {
-    //   for (let m = 2; i * m <= N; m++) {
-    //     /** increasing connections toll on connecting nodes */
-    //     this.nodes[getIndex(i)].linkCount++;
-    //     this.nodes[getIndex(i * m)].linkCount++;
-
-    //     /** connecting the nodes before starting the simulation */
-    //     this.links.push(new Link(i, i * m));
-    //   }
-    // }
-  }
+  ) {}
 
   ngOnInit() {
     const currentUser = this.authService.currentUser;
